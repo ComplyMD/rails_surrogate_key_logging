dbf7d51e649c11000e61163f6cabfeb8b575cc59
amar-bakane <109940271+amar-bakane@users.noreply.github.com>
2023-03-20 23:14:07 +0530

    Empty check (#4)

--- lib/surrogate_key_logging/key_manager.rb
+++ lib/surrogate_key_logging/key_manager.rb
@@ -36,6 +36,7 @@ module SurrogateKeyLogging
     end
     
     def call(_key, value, _parents = [], _original_params = nil)
+      return "" if value.blank?
       surrogate = get(value)
       Rails.logger.tagged('SurrogateKeyLogging') { Rails.logger.info "Surrogate: `#{surrogate}`, value: `#{value}`" } if SurrogateKeyLogging.config.debug
       surrogate
